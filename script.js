e97a94ab1f4f26dbbf1369ea1cc65657efd83b6d
Abdulaziz Chalya <abdulazizchalya@gmail.com>
2018-07-31 18:03:24 +0300

    Finished all functionality, visuals, and polishing

--- script.js
+++ script.js
@@ -1,63 +1,90 @@
-const playerScore = document.querySelector('#playerScore');
-const compScore = document.querySelector('#compScore');
+/*-------------defining all constants----------------*/
 
-const rockBtn = document.querySelector('#rockBtn')
-const paperBtn = document.querySelector('#paperBtn')
-const scissorsBtn = document.querySelector('#scissorsBtn')
+const playerScore = document.getElementById('playerScore');
+const compScore = document.getElementById('compScore');
+const reset = document.getElementById('reset');
+const tieMessage = document.getElementById('tieMessage');
 
-const message = document.querySelector('#message')
+const rock = document.getElementById('rock');
+const paper = document.getElementById('paper');
+const scissors = document.getElementById('scissors');
 
-playerScore.textContent = 0;
-compScore.textContent = 0;
+const message = document.getElementById('message');
 
-message.textContent = 'choose a weapon to start';
+var tieCounter = 0;
 
-/*function computerPlay() {
+/*-----------------adding all event listeners------------*/
+
+rock.addEventListener('click', e => playRound(e, computerPlay()));
+paper.addEventListener('click', e => playRound(e, computerPlay()));
+scissors.addEventListener('click', e => playRound(e, computerPlay()));
+
+reset.addEventListener('click', resetGame);
+
+/*--------------------functions-----------------------*/
+
+resetGame();
+
+function mouseIn(image) {
+    image.src = `${image.id}Hover.png`;
+    image.classList.add('hover');
+}
+
+function mouseOut(image) {
+    image.src = `${image.id}.png`;
+    image.classList.remove('hover');
+}
+
+function resetGame() {
+    playerScore.textContent = 0;
+    compScore.textContent = 0;
+    message.textContent = 'choose a weapon to start';
+    tieMessage.textContent = '';
+}
+
+function computerPlay() {
     var choices = ['rock', 'paper', 'scissors'];
 
     return choices[(Math.floor(Math.random() * choices.length))];
-
 }
 
-function playRound(playerSelection, computerSelection) {
-
+function playRound(e, computerSelection) {
+    //computerSelection = 'paper';
+    var playerSelection = e.target.id;
+    
     if (playerSelection == computerSelection) {
-        return 'a tie!';
+        tieMessage.style.fontWeight = 'normal';
+        tieMessage.style.textDecoration = 'none';
+        
+        if (tieCounter == 0) {
+            tieMessage.textContent = 'tie';
+            tieCounter++;
+        }
+        else if (tieCounter == 1) {
+            tieMessage.textContent = 'tie again';
+            tieCounter++;
+        }
+        else if (tieCounter == 2) {
+            tieMessage.style.fontWeight = 'bold';
+            tieCounter++;
+        }
+        else if (tieCounter == 3) {
+            tieMessage.style.fontWeight = 'bold';
+            tieMessage.style.textDecoration = 'underline';
+        }
     }
 
     else if (((playerSelection == 'rock') && (computerSelection == 'scissors')) || ((playerSelection == 'paper') && (computerSelection == 'rock')) || ((playerSelection == 'scissors') && (computerSelection == 'paper'))) {
-        return 'you win';
+        playerScore.textContent++;
+        tieMessage.textContent = '';
+        tieCounter = 0;
     }
 
     else {
-        return 'you lose';
+        compScore.textContent++;
+        tieMessage.textContent = '';
+        tieCounter = 0;
     }
-
+    
+    message.textContent = `computer chose ${computerSelection}`;
 }
-                               
-function game() {
-    var score = 0;
-
-    for (var i = 0; i < 5; i++) {
-        playerSelection = window.prompt('Rock, paper, or scissors?').toLowerCase();
-
-        result = playRound(playerSelection, computerPlay());
-        console.log(result);
-
-        if (result == 'you win') {
-            score++;
-        }
-
-        if (result == 'a tie!') {
-            i--;
-        }
-    }
-
-    if (score >= 3) {
-        console.log(score + " out of 5 is a winning score!");
-    } else {
-        console.log(score + " out of 5 is a losing score...");
-    }
-}
-            
-game();*/
